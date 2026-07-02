e4947893b579a78da38b15ce1ab3bf86f37c1827
lsh312 <lea.sarouphim@gmail.com>
2026-07-02 13:24:53 +0200

    added casting to staging layer

--- models/staging/stg_air_quality_hourly.sql
+++ models/staging/stg_air_quality_hourly.sql
@@ -1,13 +1,13 @@
 select
-    location_id,
-    city_name,
-    country_code,
-    timestamp,
-    pm10,
-    pm2_5,
-    carbon_monoxide,
-    nitrogen_dioxide,
-    ozone,
-    european_aqi,
-    extracted_at
+    location_id::integer as location_id,
+    city_name::varchar as city_name,
+    country_code::varchar as country_code,
+    timestamp::timestamp as timestamp,
+    pm10::double as pm10,
+    pm2_5::double as pm2_5,
+    carbon_monoxide::double as carbon_monoxide,
+    nitrogen_dioxide::double as nitrogen_dioxide,
+    ozone::double as ozone,
+    european_aqi::integer as european_aqi,
+    extracted_at::timestamp as extracted_at
 from {{ source('raw', 'raw_air_quality_hourly') }}
--- models/staging/stg_forecast_daily.sql
+++ models/staging/stg_forecast_daily.sql
@@ -1,14 +1,14 @@
 select
-    location_id,
-    city_name,
-    country_code,
-    date,
-    temperature_2m_max,
-    temperature_2m_min,
-    temperature_2m_mean,
-    precipitation_sum,
-    rain_sum,
-    snowfall_sum,
-    wind_speed_10m_max,
-    extracted_at
+    location_id::integer as location_id,
+    city_name::varchar as city_name,
+    country_code::varchar as country_code,
+    date::date as date,
+    temperature_2m_max::double as temperature_2m_max,
+    temperature_2m_min::double as temperature_2m_min,
+    temperature_2m_mean::double as temperature_2m_mean,
+    precipitation_sum::double as precipitation_sum,
+    rain_sum::double as rain_sum,
+    snowfall_sum::double as snowfall_sum,
+    wind_speed_10m_max::double as wind_speed_10m_max,
+    extracted_at::timestamp as extracted_at
 from {{ source('raw', 'raw_forecast_daily') }}
